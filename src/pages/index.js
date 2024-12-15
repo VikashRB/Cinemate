8354ef98ad19c1fcedaa4fbd307addaca85063b8
VikashRB <160260320+VikashRB@users.noreply.github.com>
2024-12-15 13:00:13 +0530

    Movie API Project
    React App that uses Movie API to fetch and search movie / TV series info

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import MovieDetail from "./MovieDetail";
+import MovieList from "./MovieList";
+import Search from "./Search";
+export Search
+export MovieDetail
+export MovieList
---
+++ src/routes/AllRoutes.js
@@ -0,0 +1,37 @@
+import { Routes, Route } from "react-router-dom";
+import MovieDetail from "../pages/MovieDetail";
+import MovieList from "../pages/MovieList";
+import Search from "../pages/Search";
+import PageNotFound from "../pages/PageNotFound";
+
+import React from "react";
+
+const AllRoutes = () => {
+  return (
+    <main className="dark:bg-slate-800">
+      <Routes>
+        <Route
+          path=""
+          element={<MovieList apiPath="movie/now_playing" title="Home" />}
+        />
+        <Route path="movie/:id" element={<MovieDetail />} />
+        <Route
+          path="movies/popular"
+          element={<MovieList apiPath="movie/popular" title="Popular" />}
+        />
+        <Route
+          path="movies/top"
+          element={<MovieList apiPath="movie/top_rated" title="Top Rated" />}
+        />
+        <Route
+          path="movies/upcoming"
+          element={<MovieList apiPath="movie/upcoming" title="Upcoming" />}
+        />
+        <Route path="search" element={<Search apiPath="search/movie" />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </main>
+  );
+};
+
+export default AllRoutes;
